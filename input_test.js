76f9d038e05bca4c067acd1ef28975c0f432af6d
ArturStahov <vtemy.at.ua@gmail.com>
2020-07-07 23:37:33 +0300

    fix 1

--- input_test.js
+++ input_test.js
@@ -1,12 +1,14 @@
 "use strict";
-//скрипт работы с инпутами с DOM
-let valueArray = []; // сюда будут записываться значения что вводит пользователь
+
+let valueArray = [];
 let textInputRef = document.querySelector(".user-input-form__input");
 let buttonRef = document.querySelector(".user-input-form__submit-button");
 let result_block_form = document.getElementById("result");
 let str;
 
 buttonRef.addEventListener("click", function() {
+  event.preventDefault();
+
   if (textInputRef.value === "") {
     result_block_form.innerHTML = "<h2>Введите значение!</h2>";
     result_block_form.style.display = "block";
